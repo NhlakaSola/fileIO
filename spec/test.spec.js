9c4e7970b457af17da6f40d5c40d22c763e66218
Mzito <you@example.com>
2020-01-15 10:12:42 +0200

    commit on master

---
+++ spec/test.spec.js
@@ -0,0 +1,10 @@
+let visitors =require('../src/script');
+let visitor_file = new visitors;
+
+
+describe('function save()', function(){
+  
+  it('checks if file exists', function(){
+      expect(visitor_file.save()).toBe()
+  });
+});
